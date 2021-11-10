3f7b5e216beaffc52f8cc7358745aa432457e7ad
Oualid Guennif <guennif.oualid@hotmail.com>
2021-11-10 16:44:03 +0100

    create notary and business one db

---
+++ verification_service_1/src/main/java/org/acme/domain/Personne.java
@@ -0,0 +1,133 @@
+package org.acme.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import java.time.LocalDate;
+
+@Entity
+public class Personne {
+    @Id
+    @Column(name = "num_securite_social", nullable = false, length = 15)
+    private String id;
+
+    @Column(name = "nom", nullable = false, length = 30)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false, length = 30)
+    private String prenom;
+
+    @Column(name = "rue", length = 30)
+    private String rue;
+
+    @Column(name = "numero_rue", length = 3)
+    private String numeroRue;
+
+    @Column(name = "code_postal", nullable = false, length = 5)
+    private String codePostal;
+
+    @Column(name = "date_naissance", nullable = false)
+    private LocalDate dateNaissance;
+
+    @Column(name = "lieu_naissance", length = 30)
+    private String lieuNaissance;
+
+    @Lob
+    @Column(name = "statut_maritale")
+    private String statutMaritale;
+
+    @Column(name = "telephone", length = 10)
+    private String telephone;
+
+    @Column(name = "mail", length = 50)
+    private String mail;
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getStatutMaritale() {
+        return statutMaritale;
+    }
+
+    public void setStatutMaritale(String statutMaritale) {
+        this.statutMaritale = statutMaritale;
+    }
+
+    public String getLieuNaissance() {
+        return lieuNaissance;
+    }
+
+    public void setLieuNaissance(String lieuNaissance) {
+        this.lieuNaissance = lieuNaissance;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getNumeroRue() {
+        return numeroRue;
+    }
+
+    public void setNumeroRue(String numeroRue) {
+        this.numeroRue = numeroRue;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}

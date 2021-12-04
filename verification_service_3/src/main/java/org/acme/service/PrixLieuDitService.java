1fa3770d495712c637e2f1651dcbdc44a2794456
Tibo <thibaut.coquelet@gmail.com>
2021-12-04 17:01:44 +0100

    Service 3 wip

--- verification_service_3/src/main/java/org/acme/service/PrixLieuDitService.java
+++ verification_service_3/src/main/java/org/acme/service/PrixLieuDitService.java
@@ -1,8 +1,9 @@
 package org.acme.service;
 
+import org.acme.DTO.PrixDeVenteDTO;
 import org.acme.exception.PrixLieuDitNotFound;
 
 public interface PrixLieuDitService {
     boolean comparePrixLieuDit(double prixM2BienImmobilier, int codePostal) throws PrixLieuDitNotFound;
-
+    String checkService3(PrixDeVenteDTO prixDeVenteDTO) throws PrixLieuDitNotFound;
     }

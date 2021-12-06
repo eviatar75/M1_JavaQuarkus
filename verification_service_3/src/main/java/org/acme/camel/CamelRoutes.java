17cb16879519910e6a7855d50f63551751819aa1
Tibo <thibaut.coquelet@gmail.com>
2021-12-06 01:15:48 +0100

    fix service 3 + log response

--- verification_service_3/src/main/java/org/acme/camel/CamelRoutes.java
+++ verification_service_3/src/main/java/org/acme/camel/CamelRoutes.java
@@ -8,24 +8,22 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+@ApplicationScoped
 public class CamelRoutes extends RouteBuilder {
 
     @Inject
     PrixLieuDitService plds;
 
-    @Inject
-    CamelContext context;
-
     @Override
     public void configure() throws Exception {
-        context.addService(plds);
-        context.setTracing(true);
         from("jms:queue/ServiceDeVerification3")
                 .unmarshal().json(PrixDeVenteDTO.class)
                 .log("json unmarshal to prixdeventedto")
                 .bean(plds,"checkService3")
+                .log("voici le contenu du service 3 ${body}")
                 .to("jms:queue/responseToLandService")
         ;
 

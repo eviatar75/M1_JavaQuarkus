6ab5074600191f195928dadc2eaf24ecd861f120
Oualid Guennif <71497699+OualidGuennif@users.noreply.github.com>
2021-12-07 23:14:07 +0100

    Update MailService.java

--- notary/src/main/java/org/acme/service/MailService.java
+++ notary/src/main/java/org/acme/service/MailService.java
@@ -20,7 +20,7 @@ public class MailService {
         File fichier = new File(path);
         mailer.send(
                 Mail.withText(destinaire,
-                        "Acte de vente","Bonjour M,Mme "+name+",\n\nNous revenons vers vous afin de vous informer que l'étude de votre contrat de vente est désormais terminé.\n\nVous retrouverez en pièce-jointe un pdf récapitulatif de votre Acte de vente.\n\n\nCordialement,\n\nLe notaire de Tolbiac"
+                        "Acte de vente","Bonjour Mme, M. "+name+",\n\nNous revenons vers vous afin de vous informer que l'étude de votre contrat de vente est désormais terminé.\n\nVous retrouverez en pièce-jointe un pdf récapitulatif de votre Acte de vente.\n\n\nCordialement,\n\nLe notaire de Tolbiac"
                 ).addAttachment("Acte_de_vente.pdf",fichier,"pdf")
         );
     }
